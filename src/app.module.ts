44da88d12d3613f5c190a0fc77a54c6aac6d0be7
Victor Nevola <victornevolapn@gmail.com>
2021-07-29 00:55:15 -0300

    project

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DesafiosModule } from './desafios/desafios.module';
+import { PartidasModule } from './partidas/partidas.module';
+import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
+import { ConfigModule } from '@nestjs/config'
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/srdesafios',
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    DesafiosModule,
+    PartidasModule,
+    ProxyRMQModule,
+    ConfigModule.forRoot({isGlobal: true}),
+],
+})
+export class AppModule {}
